08e498d9cffd5da0d7fc35ed8631153cbed0b6c3
髙橋 陸 <triku@netprotections.co.jp>
2020-03-07 23:35:50 +0900

    Rename spec_file

--- spec/models/evaluation_criteria_spec.rb
+++ spec/models/evaluation_criteria_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe EvaluationCriterium, type: :model do
+RSpec.describe EvaluationCriteria, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
