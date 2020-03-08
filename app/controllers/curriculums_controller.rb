81d4f660272367c927863eb209b542587a4f53ea
髙橋 陸 <triku@netprotections.co.jp>
2020-03-08 19:51:24 +0900

    Add index and show action

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -1,2 +1,25 @@
 class CurriculumsController < ApplicationController
+  def index
+    # q = params[:q] || {}
+    #
+    # @search_params = q.clone
+    #
+    # @search = Web::Np::IrregularReceiptsSearchForm.new(q)
+
+    # if params[:q].present?
+    #   @results = @search.search_irregular_receipts(q)
+    #   @curriculums = @results.page(params[:page]).per(20)
+    # end
+
+    # 一覧ではいくつかのカテゴリに分けて表示したい
+    # ①成績分布がいいやつ
+    # ②
+    @search = Curriculum.ransack(params[:q])
+    @curriculums = @search.result
+    @curriculums = Curriculum.all unless @curriculums
+  end
+
+  def show
+    @curriculum = Curriculum.find(params[:id])
+  end
 end
