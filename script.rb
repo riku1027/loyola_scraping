66e1c4d3d66ac063880485ba9564a59fc28582df
riku1027 <45778163+riku1027@users.noreply.github.com>
2021-03-20 18:41:27 +0900

    Refactor

--- script.rb
+++ script.rb
@@ -1,6 +1,33 @@
 require './sophian'
 require './lib/exporter'
-require 'pry'
+require './lib/grade_exporter'
+
+def crawl_syllabus(sophian)
+  sophian.crawl_syllabus_from_loyola
+  crawl_results = sophian.crawler_instance.crawl_results
+  logging_crawl_result(crawl_results)
+  export_crawl_result(crawl_results, SyllabusExporter.new)
+end
+
+def crawl_grade(sophian)
+  sophian.crawl_grade_from_loyola
+  crawl_results = sophian.crawler_instance.crawl_results
+  logging_crawl_result(crawl_results)
+  export_crawl_result(crawl_results, GradeExporter.new)
+end
+
+def logging_crawl_result(crawl_results)
+  puts <<-EOS
+  "#{crawl_results}"
+  スクレイピングが完了しました。
+  EOS
+end
+
+def export_crawl_result(crawl_results, exporter_instance)
+  puts '何か文字を入力するとエクスポートを開始します。'
+  exporter_instance.execute(crawl_results) if $stdin.gets.chomp
+end
+
 
 sophian = Sophian.new(id: ENV['LOYOLA_LOGIN_ID'], password: ENV['LOYOLA_LOGIN_PASSWORD'])
 
@@ -9,25 +36,14 @@ puts <<EOS
 [1]：成績情報をスクレイピング
 EOS
 
-got_num = $stdin.gets.chomp.to_i
+got_num = $stdin.gets.chomp
 
 case got_num
-when 0
-  sophian.crawl_syllabus_from_loyola
-when 1
-  sophian.crawl_grade_from_loyola
+when "0"
+  crawl_syllabus(sophian)
+when "1"
+  crawl_grade(sophian)
 else
   puts '不正な数字です。処理を終了します。'
   exit
 end
-
-crawl_results = sophian.crawler_instance.crawl_results
-puts crawl_results
-puts <<EOS
-スクレイピングが完了しました。
-何か文字を入力するとエクスポートを開始します。
-EOS
-
-got_num = $stdin.gets.chomp
-
-Exporter.new.execute(crawl_results) if got_num
